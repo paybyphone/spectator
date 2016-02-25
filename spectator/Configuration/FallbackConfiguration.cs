47a044732bac05be5561d2cff1e7b9a4b1c43b7c
Phillip Wong <pwong@paybyphone.com>
2016-02-25 14:29:34 -0800

    Added logging to fallback.

--- spectator/Configuration/FallbackConfiguration.cs
+++ spectator/Configuration/FallbackConfiguration.cs
@@ -5,17 +5,20 @@ namespace spectator.Configuration
 {
     public static class Fallback
     {
+        private static readonly ILog Log = LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
+
         public static ISpectatorConfiguration On(Func<ISpectatorConfiguration> configFactory, Func<ISpectatorConfiguration> fallbackFactory)
         {
             try
             {
                 return configFactory();
             }
-            catch (Exception)
+            catch (Exception ex)
             {
+                Log.Warn("Couldn't obtain primary configuration, falling back to secondary configuration", ex);
+
                 return fallbackFactory();
             }
         }
-
     }
 }
