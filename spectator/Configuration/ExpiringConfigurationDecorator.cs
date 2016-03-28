ce6278011448f546ecce43e752872f3006705c12
Phillip Wong <pwong@paybyphone.com>
2016-03-28 15:31:38 -0700

    Refactoring.

--- spectator/Configuration/ExpiringConfigurationDecorator.cs
+++ spectator/Configuration/ExpiringConfigurationDecorator.cs
@@ -49,7 +49,7 @@ namespace spectator.Configuration
                 {
                     var diff = new ConfigurationDifferenceSummary(_configurationInstance, newConfig);
 
-                    if (!string.IsNullOrEmpty(diff.ToString()))
+                    if (!diff.IsEmpty())
                     {
                         Log.Info("Configuration changed: \n" + diff);
                     }
