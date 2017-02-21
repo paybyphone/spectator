518d1c33b963478cf08b79d06cd6689da11c6cac
Tommy Parnell <tommy@terribledev.io>
2017-02-21 09:46:03 -0500

    add install file

---
+++ installers/chocolatey/spectator/tools/chocolateyinstall.ps1
@@ -0,0 +1,14 @@
+﻿$packageName= 'Spectator'
+$toolsDir   = "$(Split-Path -Parent $MyInvocation.MyCommand.Definition)"
+$fileLocation = Join-Path $toolsDir 'Spectator.msi'
+
+$packageArgs = @{
+  packageName   = $packageName
+  fileType      = 'msi'
+  file          = $fileLocation
+  silentArgs    = "/qn /norestart"
+  validExitCodes= @(0, 3010, 1641)
+  softwareName  = 'Spectator*'
+}
+
+Install-ChocolateyInstallPackage @packageArgs
